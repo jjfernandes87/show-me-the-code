b616f21dc37f1f2a91e80b07bb29ac8a5fd72432
Julio Fernandes <jfjunior@zapcorp.com.br>
2018-02-21 21:58:03 -0300

    changing name Car to CarRepair

---
+++ AppChallenge/Entities/CarRepair.swift
@@ -0,0 +1,13 @@
+//
+//  CarRepair.swift
+//  AppChallenge
+//
+//  Created by Julio Fernandes on 21/02/18.
+//  Copyright © 2018 Julio Fernandes. All rights reserved.
+//
+
+import UIKit
+
+struct CarRepair {
+    
+}
