3e0f35914e718ce39a36764b5d1d4ae76ee20da9
Julio Fernandes <jfjunior@zapcorp.com.br>
2018-02-21 21:13:05 -0300

    create files for VIPER architecture

---
+++ AppChallenge/Interactors/PlacesInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  PlacesInteractor.swift
+//  AppChallenge
+//
+//  Created by Julio Fernandes on 21/02/18.
+//  Copyright © 2018 Julio Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class PlacesInteractor: NSObject {
+
+}
