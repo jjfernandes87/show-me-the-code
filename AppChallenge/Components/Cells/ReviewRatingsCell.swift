b3dd1ac3d6bb3c17eb781667721de95a3459f1a8
Julio Fernandes <jfjunior@zapcorp.com.br>
2018-02-26 20:02:33 -0300

    Fix: Adjust layout for progress bar

--- AppChallenge/Components/Cells/ReviewRatingsCell.swift
+++ AppChallenge/Components/Cells/ReviewRatingsCell.swift
@@ -21,11 +21,20 @@ class ReviewRatingsCell: CellController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = loadDefaultCellForTable(tableView: tableView, atIndexPath: indexPath) as! ReviewRatingsCellView
         cell.ratingValue.text = String(format: "%.1f", data.rating ?? 0)
+        cell.fiveStarsView.progress = 1
+        cell.fourStarsView.progress = 0.8
+        cell.threeStarsView.progress = 0.6
+        cell.twoStarsView.progress = 0.4
+        cell.oneStarsView.progress = 0.2
         return cell
     }
-    
 }
 
 class ReviewRatingsCellView: CellView {
     @IBOutlet weak var ratingValue: UILabel!
+    @IBOutlet weak var fiveStarsView: UIProgressView!
+    @IBOutlet weak var fourStarsView: UIProgressView!
+    @IBOutlet weak var threeStarsView: UIProgressView!
+    @IBOutlet weak var twoStarsView: UIProgressView!
+    @IBOutlet weak var oneStarsView: UIProgressView!
 }
